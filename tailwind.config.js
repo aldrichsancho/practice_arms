5f63b6ad04633d99d9b754058fafe44b33a39735
Axel Shira Sapata Negara <71739351+axelnegara07@users.noreply.github.com>
2022-02-27 16:37:12 +0800

    tidying

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    color: {
+      transparent: "transparent",
+      current: "currentColor",
+      basic:{
+        50: "#FF685B"
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
