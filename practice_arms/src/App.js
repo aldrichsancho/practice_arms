bd40fdf3a37877124cb0def847ccac0bfce2eec3
EdwinSitumorang <1872051@maranatha.ac.id>
2022-02-27 14:16:03 +0700

    Tambah Komponen Footer

--- practice_arms/src/App.js
+++ practice_arms/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/header';
+import Footer from './component/Footer';
 
 function App() {
   return (
--- practice_arms/src/index.js
+++ practice_arms/src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './component/header';
 import FoodItem from './component/FoodItem';
+import Footer from './component/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <FoodItem/>
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
